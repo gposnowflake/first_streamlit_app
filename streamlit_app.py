0c01c733651b4fc1a8b9ac76b35621d13e16bd5b
gposnowflake <109813781+gposnowflake@users.noreply.github.com>
2022-07-29 16:48:30 +0200

    Update streamlit_app.py
    added snowflake function and first part of snowflake section

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,11 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+
 # init snowflake connection at the top to be reusable
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -51,11 +56,19 @@ except URLError as e:
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding', add_my_fruit)
 
+
+# Snowflake section
+
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  fruit_list_data_rows = get_fruit_load_list()
+  streamlit.dataframe(fruit_list_data_rows)
+
 # stops executing the rest of the app
 streamlit.stop()
 
 # this will not work yet
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
